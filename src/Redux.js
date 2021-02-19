b0035b33dabc5f4199c12954aadbb89b9431d87c
paweldyl <hakiros54@gmail.com>
2021-02-19 22:35:04 +0100

    fixing bugs

--- src/Redux.js
+++ src/Redux.js
@@ -6,28 +6,28 @@ const initialState = {
 	dark_theme: false,
 	dictionary: {
 		pl: {
-			about_me: "O mnie"
-			exams: "Egzaminy"
-			skills: "Umiejętności"
-			projects: "Projekty"
-			books: "Ksiązki"
-			resume: "Cv"
-			contact: "Kontakt"
-			about_me: "Kim jestem"
+			about_me: "O mnie",
+			exams: "Egzaminy",
+			skills: "Umiejętności",
+			projects: "Projekty",
+			books: "Ksiązki",
+			resume: "Cv",
+			contact: "Kontakt",
+			about_me: "Kim jestem",
 			about_me_text: "Jestem początkującym Web Developerem po technikum informatycznym. Przez rok studiowałem informatykę dzienną na Politechnice Śląskiej lecz zrezygnowałem gdyż studia nie pokryły się z moimi zainteresowaniami. Planuje zostać Front-end lub Full-stack Developerem. Aktualnie głównie uczę się Reacta z Reduxem ale w planach mam także opanowanie Node.js oraz Typescript. Opanowałem także podstawy machine learningu i gdyby była taka możliwość interesuje mnie rozwój także w tym kierunku.",
 			math: "Matematyka podstawowa",
 			extended_math: "Matematyka rozszerzona",
 
 		}
 		en: {
-			about_me: "About me"
-			exams: "Exams"
-			skills: "Skills"
-			projects: "Projects"
-			books: "Books"
-			resume: "Resume"
-			contact: "Contact"
-			about_me: "About me"
+			about_me: "About me",
+			exams: "Exams",
+			skills: "Skills",
+			projects: "Projects",
+			books: "Books",
+			resume: "Resume",
+			contact: "Contact",
+			about_me: "About me",
 			about_me_text: "I am a Web Developer after an IT technical college. For a year I studied computer science at the Silesian University of Technology,  then quit because it did not coincide with my interests. I plan to become a Front-end or Full-stack Developer. Currently, I am mainly learning React with Redux, but I also plan to master Node.js and Typescript. I also mastered the basics of machine learning and if there was such a possibility, I am interested in developing also in this direction.",
 			math: "Math",
 			extended_math: "Extended math",
