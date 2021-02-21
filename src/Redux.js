f997b2e3cda824f2eeff13b6cc9ae88c356d25c6
paweldyl <hakiros54@gmail.com>
2021-02-21 23:03:41 +0100

    completed dictionary

--- src/Redux.js
+++ src/Redux.js
@@ -17,8 +17,22 @@ const initialState = {
 			about_me_text: "Jestem początkującym Web Developerem po technikum informatycznym. Przez rok studiowałem informatykę dzienną na Politechnice Śląskiej lecz zrezygnowałem gdyż studia nie pokryły się z moimi zainteresowaniami. Planuje zostać Front-end lub Full-stack Developerem. Aktualnie głównie uczę się Reacta z Reduxem ale w planach mam także opanowanie Node.js oraz Typescript. Opanowałem także podstawy machine learningu i gdyby była taka możliwość interesuje mnie rozwój także w tym kierunku.",
 			math: "Matematyka podstawowa",
 			extended_math: "Matematyka rozszerzona",
-
-		}
+			english: "Angielski",
+			extended_english: "Angielski rozszerzony",
+			spoken_english: "Angielski ustny",
+			e12_theory: "E12 teoria",
+			e12_practic: "E12 Praktyka",
+			e13_theory: "E13 teoria",
+			e13_practic: "E13 Praktyka",
+			e14_theory: "E14 teoria",
+			e14_practic: "E14 Praktyka",
+			clean_code: "Czysty kod",
+			clean_code_description: "książka pokazuje czym powinien charakteryzować się czysty kod i jak go pisać.",
+			software_craftsman: "software Craftsman",
+			software_craftsman_description: "Książka ta proponuje czysto rzemieślnicze podejście do pracy programisty. Idea ta, znana jako software craftsmanship, zakłada kompletną zmianę sposobu myślenia o programowaniu. Priorytetami stają się tu profesjonalizm, techniczna perfekcja i zadowolenie klienta, a osiąga się to, stosując pewne praktyki programistyczne, które zaczerpnięto m.in. z metodyk programowania ekstremalnego, zwinnego i odchudzonego.",
+			responsive_web_design_description: "Responsive Web Design",
+			responsive_web_design_description: "Książka uczy jak tworzyć responsywne strony internetowe dostosowane do każdych urządzeń."
+		},
 		en: {
 			about_me: "About me",
 			exams: "Exams",
@@ -35,11 +49,17 @@ const initialState = {
 			extended_english: "Extended English",
 			spoken_english: "Spoken English",
 			e12_theory: "E12 Theory",
-			e12_practic: "E12 Practice"
+			e12_practic: "E12 Practice",
 			e13_theory: "E13 Theory",
-			e13_practic: "E13 Practice"
+			e13_practic: "E13 Practice",
 			e14_theory: "E14 Theory",
-			e14_practic: "E14 Practice"
+			e14_practic: "E14 Practice",
+			clean_code: "Clean code",
+			clean_code_description: "Clean code is about what is and how to write clean code.",
+			software_craftsman: "software Craftsman",
+			software_craftsman_description: "Książka ta proponuje czysto rzemieślnicze podejście do pracy programisty. Idea ta, znana jako software craftsmanship, zakłada kompletną zmianę sposobu myślenia o programowaniu. Priorytetami stają się tu profesjonalizm, techniczna perfekcja i zadowolenie klienta, a osiąga się to, stosując pewne praktyki programistyczne, które zaczerpnięto m.in. z metodyk programowania ekstremalnego, zwinnego i odchudzonego.",
+			responsive_web_design_description: "Responsive Web Design",
+			responsive_web_design_description: "Książka uczy jak tworzyć responsywne strony internetowe dostosowane do każdych urządzeń."
 		}
 	}
 }
