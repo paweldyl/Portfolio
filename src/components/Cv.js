f881e3abbd2b0b00559d1c7e6a68f1a76ddd995d
paweldyl <hakiros54@gmail.com>
2021-02-28 21:22:27 +0100

    now english version of page download english resume

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -6,7 +6,7 @@ const Cv = () => {
 	const dictionary = useSelector(state => state.dictionary);
 	return (
 		<section id="cv" data-aos="fade-up">
-			<a href="CV_Paweł_Dyl.pdf" download>{dictionary[language]["download_resume"]}</a>
+			<a href={language === "pl" ? "CV_Paweł_Dyl.pdf" : "Paweł_Dyl_Resume.pdf"} download>{dictionary[language]["download_resume"]}</a>
 		</section>
 	)
 }
